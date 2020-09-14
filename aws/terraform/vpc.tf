e45eaefbb89eb0897858e01bf22a5d0bec7fa2f9
xavier <xmendez@flip-corp.com>
2020-09-14 06:16:58 -0600

    Agregando default vpc y subnet 1

---
+++ aws/terraform/vpc.tf
@@ -0,0 +1,61 @@
+# DEFAULT VPC
+resource "aws_vpc" "default_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  tags = {
+    Name = "default_vpc"
+  }
+}
+
+# INTERNET GATEWAY
+resource "aws_internet_gateway" "default-igw" {
+  vpc_id = aws_vpc.default_vpc.id
+  tags = {
+    Name = "default-igw"
+  }
+}
+
+# SUBNET 1 us-east-2a
+resource "aws_subnet" "us_east_2a_subnet_1" {
+  map_public_ip_on_launch = true
+  availability_zone       = element(var.az_names, 0)
+  vpc_id                  = aws_vpc.default_vpc.id
+  cidr_block              = element(var.subnet_cidr_blocks, 0)
+  tags = {
+    "Name" = "us_east_2a_subnet_1"
+  }
+}
+
+# ELASTIC IP
+resource "aws_eip" "EIP_01" {
+}
+
+
+# NAT GATEWAY
+resource "aws_nat_gateway" "us_east_2a_NG" {
+  subnet_id     = aws_subnet.us_east_2a_subnet_1.id
+  allocation_id = aws_eip.EIP_01.id
+  tags = {
+    "Name" = "us_east_2a_NG"
+  }
+}
+
+# ROUTE TABLE 
+resource "aws_route_table" "route_table_public" {
+  vpc_id = aws_vpc.default_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.default-igw.id
+  }
+  tags = {
+    "Name" = "route_table_public"
+  }
+}
+
+# ROUTE TABLE ASSOCIATION
+resource "aws_route_table_association" "PUBLIC_ASSO" {
+  route_table_id = aws_route_table.route_table_public.id
+  subnet_id      = aws_subnet.us_east_2a_subnet_1.id
+}
+
+
